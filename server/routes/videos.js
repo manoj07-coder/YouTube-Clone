7eb7f15cbf2dd339dea209e4116ef5827ce72a5c
Manoj <manojgowdacs35@gmail.com>
2024-01-03 23:41:08 +0530

    video routes

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import { verifyToken } from '../verifyToken.js';
-import { addVideo, deleteVideo, getVideo, updateVideo } from '../controllers/video.js';
+import { addVideo, addView, deleteVideo, getBySearch, getByTag, getVideo, random, sub, trend, updateVideo } from '../controllers/video.js';
 
 const router = express.Router()
 
 router.post('/',verifyToken,addVideo);
 router.put('/:id',verifyToken,updateVideo);
 router.delete('/:id',verifyToken,deleteVideo);
-router.post('/find/:id',getVideo);
-router.put('/view/:id',getVideo);
-router.get('/trend',getVideo);
-router.get('/random',getVideo);
-router.get('/sub',getVideo);
+router.get('/find/:id',getVideo);
+router.put('/view/:id',addView);
+router.get('/trend',trend);
+router.get('/random',random);
+router.get('/sub',verifyToken,sub);
+router.get('/tags',getByTag);
+router.get('/search',getBySearch);
+
 
 export default router;
